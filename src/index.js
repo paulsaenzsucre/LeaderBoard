0c43eecc4400cb9e25ac424cead75ed6bc09f007
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-22 09:08:04 -0500

    Add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './style.css';
 import ScorePresenter from './modules/ScorePresenter.js';
+import ScoreBoardView from './modules/ScoreBoardView';
 
-const container = document.getElementById('score-list');
-const presenter1 = new ScorePresenter(container, 'Paul', 100);
+const container = document.getElementById('container');
+const presenter = new ScoreBoardView(container, {});
+presenter.render();
+/* const presenter1 = new ScorePresenter(container, 'Paul', 100);
 const presenter2 = new ScorePresenter(container, 'Juan', 50);
 const presenter3 = new ScorePresenter(container, 'David', 70);
 
 presenter1.renderView();
 presenter2.renderView();
-presenter3.renderView();
+presenter3.renderView(); */
