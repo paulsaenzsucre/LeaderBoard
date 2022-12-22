802233494c57ba8bacea9879fcf78308d3098641
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-22 07:10:50 -0500

    Add ScoreView class and basic style for it

---
+++ src/modules/ScoreView.js
@@ -0,0 +1,37 @@
+class ScoreView {
+  #container;
+
+  #presenter;
+
+  #ui;
+
+  constructor(container, presenter) {
+    this.#container = container;
+    this.#presenter = presenter;
+    this.#ui = this.#createDOMElements();
+  }
+
+  render = () => {
+    this.#container.appendChild(this.#ui);
+  }
+
+  #createDOMElements = () => {
+    let element;
+    const div = document.createElement('div');
+    div.setAttribute('class', 'score-card');
+    element = document.createElement('p');
+    element.innerText = this.#presenter.getName;
+    element.setAttribute('class', 'score-name');
+    div.appendChild(element);
+    element = document.createElement('p');
+    element.innerText = this.#presenter.getPoints;
+    element.setAttribute('class', 'score-points');
+    div.appendChild(element);
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class','score-list-item');
+    listItem.appendChild(div);
+    return listItem;
+  }
+}
+
+export default ScoreView;
