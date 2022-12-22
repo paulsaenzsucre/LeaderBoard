f01c7e4738fdbb75f66aedce13c0c51281340ac0
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-22 07:16:00 -0500

    Testing ScoreView and ScorePresenter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import ScorePresenter from './modules/ScorePresenter.js';
+import ScoreView from './modules/ScoreView.js';
+
+const container = document.getElementById('score-list');
+const presenter = new ScorePresenter();
+const view = new ScoreView(container, presenter);
+const view2 = new ScoreView(container, presenter);
+const view3 = new ScoreView(container, presenter);
+view.render();
+view2.render();
+view3.render();
