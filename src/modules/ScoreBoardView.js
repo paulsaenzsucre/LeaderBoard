889670ec835e16df3cce61f95108924452b8fe5e
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-22 12:31:05 -0500

    Refactor ScoreBoardView class and style.

--- src/modules/ScoreBoardView.js
+++ src/modules/ScoreBoardView.js
@@ -1,6 +1,8 @@
 class ScoreBoardView {
   #presenter;
 
+  #listUi;
+
   #ui;
 
   constructor(presenter) {
@@ -12,20 +14,38 @@ class ScoreBoardView {
     return this.#ui;
   }
 
-  addScoreViewUi = (scoreViewUi) => this.#ui.appendChild(scoreViewUi);
+  addScoreViewUi = (scoreViewUi) => this.#listUi.appendChild(scoreViewUi);
 
   #createDOMElements = () => {
-    const scoreList = document.createElement('ul');
-    scoreList.setAttribute('class', 'scoreboard');
+    const section = document.createElement('section');
+    section.setAttribute('class', 'scoreboard-cont');
+    const heading = document.createElement('div');
+    heading.setAttribute('class', 'heading-cont');
+    const title = document.createElement('h2');
+    title.setAttribute('class', 'scoreboard-title');
+    title.innerText = 'Recent scores';
+    heading.appendChild(title);
+    const btn = document.createElement('button');
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('class', 'refresh-btn');
+    btn.innerText = 'Refresh';
+    btn.addEventListener('click', this.#refresh());
+    heading.appendChild(btn);
+    this.#listUi = document.createElement('ul');
+    this.#listUi.setAttribute('class', 'scoreboard');
 
     if (this.#presenter.model.length > 0) {
       this.#presenter.model.forEach(
-        (scorePresenter) => scoreList.appendChild(scorePresenter.view.ui),
+        (scorePresenter) => this.#listUi.appendChild(scorePresenter.view.ui),
       );
     }
 
-    return scoreList;
+    section.appendChild(heading);
+    section.appendChild(this.#listUi);
+    return section;
   }
+
+  #refresh = () => {}
 }
 
 export default ScoreBoardView;
