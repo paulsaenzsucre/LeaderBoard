8bde3886f6c90dc841b0c5fdab0ded20f8b87828
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-22 07:50:13 -0500

    Testing Score classes.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './style.css';
 import ScorePresenter from './modules/ScorePresenter.js';
-import ScoreView from './modules/ScoreView.js';
 
 const container = document.getElementById('score-list');
-const presenter = new ScorePresenter();
-const view = new ScoreView(container, presenter);
-const view2 = new ScoreView(container, presenter);
-const view3 = new ScoreView(container, presenter);
-view.render();
-view2.render();
-view3.render();
+const presenter1 = new ScorePresenter(container, 'Paul', 100);
+const presenter2 = new ScorePresenter(container, 'Juan', 50);
+const presenter3 = new ScorePresenter(container, 'David', 70);
+
+presenter1.renderView();
+presenter2.renderView();
+presenter3.renderView();
